c51b4e38d77f859a0873b483a765b79d6cc03064
julia <julia.zhyvylo@asila.mk>
2025-01-17 11:19:53 +0100

    task 4 done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`The tenant entered the house.`);
+    } else {
+      console.log("The door is closed. You cannot enter.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("The key is wrong.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
